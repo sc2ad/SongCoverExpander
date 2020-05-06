d03a8be69478e584d3c7944d9c274f9bdd6cc1fa
LDeathh <42755317+LDeathh@users.noreply.github.com>
2020-05-06 18:05:56 +0100

    Add files via upload

---
+++ include/main.hpp
@@ -0,0 +1,8 @@
+#include <dlfcn.h>
+#include "../extern/beatsaber-hook/shared/utils/utils.h"
+#include "../extern/beatsaber-hook/shared/utils/logging.h"
+#include "../extern/beatsaber-hook/shared/utils/typedefs.h"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+
+
+extern "C" void load();
